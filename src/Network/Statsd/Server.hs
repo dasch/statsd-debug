5b763acac52163a6c3aeeabefdd3b2a6adf4249b
Daniel Schierbeck <dasch@zendesk.com>
2014-05-12 19:15:12 +0000

    Rewrite the server to use channels

--- src/Network/Statsd/Server.hs
+++ src/Network/Statsd/Server.hs
@@ -1,20 +1,31 @@
 import Network.Socket
 import Network.Statsd
 import Network.Statsd.Parser
+import Control.Concurrent
+import Control.Concurrent.Chan
 import Data.Either
 
 port = "8125"
 
 main = do
+    chan <- newChan
+    forkIO $ networkServer chan
+    metricServer chan
+
+metricServer chan = do
+    metric <- readChan chan
+    print metric
+    metricServer chan
+
+networkServer chan = do
     addrinfos <- getAddrInfo (Just (defaultHints {addrFlags = [AI_PASSIVE]})) Nothing (Just port)
     let serveraddr = head addrinfos
     sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
     bindSocket sock (addrAddress serveraddr)
-    handleMessage sock
-
-handleMessage sock = do
-    (msg, _, addr) <- recvFrom sock 1024
-    case parseMetrics msg of
-        Left error -> print $ "ERROR: " ++ msg
-        Right metrics -> print metrics
-    handleMessage sock
+    let handleMessage = do
+            (msg, _, addr) <- recvFrom sock 1024
+            case parseMetrics msg of
+                Left error -> print $ "ERROR: " ++ msg
+                Right metrics -> writeList2Chan chan metrics
+            handleMessage
+    handleMessage
