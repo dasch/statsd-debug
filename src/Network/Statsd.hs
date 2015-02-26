772a5fddd6ac90978c7ff361f5233f88a76f547a
Daniel Schierbeck <dasch@zendesk.com>
2015-02-26 00:06:03 +0000

    Format metrics nicely

--- src/Network/Statsd.hs
+++ src/Network/Statsd.hs
@@ -1,10 +1,26 @@
 module Network.Statsd where
 
-import Data.Map (Map)
+import Data.List (intercalate)
+import qualified Data.Map as Map
 
 type Name = String
 type Value = Double
-type Tags = Map String String
+type Tags = Map.Map String String
 
 data MetricType = Gauge | Counter | Timer deriving (Show, Eq)
-data Metric = Metric MetricType Name Value Tags deriving (Show, Eq)
+data Metric = Metric MetricType Name Value Tags deriving Eq
+
+instance Show Metric where
+    show (Metric metricType metricName metricValue metricTags) =
+        intercalate " " [formatType metricType, formatName metricName, formatValue metricValue, formatTags metricTags]
+
+formatType = show
+formatName name = "`" ++ name ++ "'"
+formatValue = show
+
+formatTags tags =
+    let
+        formatTag (name, value) = name ++ ":" ++ value
+        formattedTags = map formatTag $ Map.toList tags
+    in
+        intercalate ", " formattedTags
